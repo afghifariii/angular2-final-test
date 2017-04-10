0d307cae21eee6871af317b23cc59ae043fab605
Ali Firdaus Ghifari <alifirdausghifari@gmail.com>
2017-04-10 13:49:27 +0700

    to-do form for adding new list

---
+++ src/app/to-do-form/to-do-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, Inject } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
+import { Router } from "@angular/router";
+
+import { ToDoListService } from "../to-do-list/to-do-list.service";
+import { lookupListToken } from "../provider";
+
+
+@Component({
+  selector: 'to-do-form',
+  templateUrl: './to-do-form.component.html',
+  styleUrls: ['./to-do-form.component.css']
+})
+export class ToDoFormComponent {
+  form;
+
+  constructor(private formBuilder: FormBuilder,
+    private toDoListService: ToDoListService,
+    @Inject(lookupListToken) public lookupLists,
+    private router: Router) { }
+
+  ngOnInit() {
+    this.form = this.formBuilder.group({
+      content: this.formBuilder.control('', Validators.compose([Validators.required, Validators.pattern('[\\w\\-\\s\\/]+')])),
+      priority: this.formBuilder.control('Normal')
+    });
+  }
+
+  onSubmit(noteItem) {
+    this.toDoListService.add(noteItem)
+    .subscribe(() => {
+      this.router.navigate(['/'])
+    });
+  }
+}
